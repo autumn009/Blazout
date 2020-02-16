d18d4d1163305e6e2cd36075242e663d23ebd1a3
Akira Kawamata <autumn@piedey.co.jp>
2020-02-16 10:39:26 +0900

    add VVRAM

---
+++ Blazout1/Blazout1Core/GameMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazout1Core
+{
+    public class VVRAM
+    {
+        public const int vvramWidth = 40;
+        public const int vvramHeight = 25;
+
+        private char[,] vvram = new char[vvramWidth, vvramHeight];
+        public char GetChar(int x, int y)
+        {
+            return vvram[x, y];
+        }
+        public void SetChar(int x, int y, char ch)
+        {
+            vvram[x, y] = ch;
+        }
+
+        public VVRAM()
+        {
+            for (int y = 0; y < vvramHeight; y++)
+            {
+                for (int x = 0; x < vvramWidth; x++)
+                {
+                    vvram[x, y] = '　';
+                }
+            }
+        }
+    }
+
+    public class GameMain
+    {
+        public VVRAM Vvram = new VVRAM();
+
+
+
+
+    }
+}
