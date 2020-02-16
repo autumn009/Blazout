06099a5dfaf0456bb53715bde337e4188fee3beb
Akira Kawamata <autumn@piedey.co.jp>
2020-02-16 11:30:40 +0900

    draw puddle

--- Blazout1/Blazout1Core/GameMain.cs
+++ Blazout1/Blazout1Core/GameMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Blazout1Core
 {
@@ -19,13 +20,13 @@ namespace Blazout1Core
             vvram[x, y] = ch;
         }
 
-        public VVRAM()
+        public void FillScreen(char ch)
         {
             for (int y = 0; y < vvramHeight; y++)
             {
                 for (int x = 0; x < vvramWidth; x++)
                 {
-                    vvram[x, y] = '　';
+                    vvram[x, y] = ch;
                 }
             }
         }
@@ -34,9 +35,54 @@ namespace Blazout1Core
     public class GameMain
     {
         public VVRAM Vvram = new VVRAM();
+        public const char WakuChar = '■';
+        public const char BlockChar = '□';
+        public const char SpaceChar = '　';
+        public const char BallChar = '●';
+        public int paddleX = 12;
+        public int paddleWidth = 5;
+        public const int timerInterval = 300;
 
+        public void DrawPaddle(int xbegin, int width)
+        {
+            int y = VVRAM.vvramHeight - 1;
+            for (int x = 0; x < VVRAM.vvramHeight; x++)
+            {
+                Vvram.SetChar(x, y, SpaceChar);
+            }
+            for (int i = 0; i < width; i++)
+            {
+                Vvram.SetChar(i+xbegin, y, WakuChar);
+            }
+        }
+        public void DrawPaddle()
+        {
+            DrawPaddle(paddleX, paddleWidth);
+        }
 
+        public void TimerProc(Task task)
+        {
+            DrawPaddle();
 
+            _ = Task.Delay(timerInterval).ContinueWith(TimerProc);
+        }
 
+        public GameMain()
+        {
+            Vvram.FillScreen(SpaceChar);
+            // draw waku line
+            for (int y = 0; y < VVRAM.vvramHeight; y++)
+            {
+                Vvram.SetChar(0, y, WakuChar);
+                Vvram.SetChar(VVRAM.vvramWidth - 1, y, WakuChar);
+            }
+            for (int x = 0; x < VVRAM.vvramWidth; x++)
+            {
+                Vvram.SetChar(x, 0, WakuChar);
+            }
+            DrawPaddle();
+
+            _ = Task.Delay(timerInterval).ContinueWith(TimerProc);
+        }
     }
 }
